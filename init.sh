9d54a7a680146c043094a0d14ef6ad1b1a96d5d6
Michał Engel <mengel@networkedassets.org>
2024-12-17 16:47:27 +0100

    brewfile, init script

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script inits yadm and the setup
+
+echo "Installing homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo >> /Users/$USER/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+echo "Installing yadm"
+brew install yadm
+
+echo "Installing git-credentials"
+brew install --cask git-credential-manager
+
+git-credential-manager github login
+git-credential-manager configure
+
+yadm clone https://github.com/efodiks/dotfiles
